a32bc490bf07150c186ac809091c2f892f4698e2
laurafrancano <55483841+laurafrancano@users.noreply.github.com>
2019-09-18 11:29:31 +0200

    Update bonjour.py

--- bonjour.py
+++ bonjour.py
@@ -6,5 +6,6 @@ aux étudiant-e-s
 ### déclaration des variables
 ### initialisation des variables
 ### séquence d'opérations
-print (" Bonjour !")
+print (" Bonjour HEG !")
 print (" Bienvenue aux étudiant -e-s 2019-2020 !")
+print (" Informatique de Gestion")
