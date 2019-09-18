01d61c1bdee5b16cc4c48a4d62a22f4d88ef31cb
Francano Laura (HES) <laura.francano@hes.adhes.hesge.ch>
2019-09-18 10:50:51 +0200

    First Save

---
+++ bonjour.py
@@ -0,0 +1,10 @@
+""" bienvenue
+données : le semestre d'entrée
+résultat : affiche un message de bienvenue
+aux étudiant-e-s
+"""
+### déclaration des variables
+### initialisation des variables
+### séquence d'opérations
+print (" Bonjour !")
+print (" Bienvenue aux étudiant -e-s 2019-2020 !")
